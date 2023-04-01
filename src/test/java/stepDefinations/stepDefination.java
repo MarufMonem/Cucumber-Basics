aca7d629ab688dadae9d222e3264d9f48f7ec934
Maruf_Monem <marufmonem@gmail.com>
2023-04-01 14:06:33 +0600

    Using tidyGherkin

--- src/test/java/stepDefinations/stepDefination.java
+++ src/test/java/stepDefinations/stepDefination.java
@@ -1,10 +1,33 @@
 package stepDefinations;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.junit.Cucumber;
+import org.junit.runner.RunWith;
 
-public class stepDefination {
+@RunWith(Cucumber.class)
+public class stepDefinitions {
+
     @Given("^User is on NetBanking landing page$")
-    public void user_is_on_NetBanking_landing_page(){
-
+    public void user_is_on_netbanking_landing_page() throws Throwable {
+//        throw new PendingException();
     }
+
+    @When("^User logs into the application with username and password$")
+    public void user_logs_into_the_application_with_username_and_password() throws Throwable {
+//        throw new PendingException();
+    }
+
+    @Then("^Home page is populated$")
+    public void home_page_is_populated() throws Throwable {
+//        throw new PendingException();
+    }
+
+    @And("^Cards are displayed$")
+    public void cards_are_displayed() throws Throwable {
+//        throw new PendingException();
+    }
+
 }

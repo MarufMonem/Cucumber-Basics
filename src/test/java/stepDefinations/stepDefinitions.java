13664f726f2b875ab8150fdbff2b5b54cc82dca6
Maruf_Monem <marufmonem@gmail.com>
2023-04-01 14:47:06 +0600

    Created and ran the test runner file

--- src/test/java/stepDefinations/stepDefinitions.java
+++ src/test/java/stepDefinations/stepDefinitions.java
@@ -7,27 +7,31 @@ import io.cucumber.java.en.When;
 import io.cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
+
 public class stepDefinitions {
 
     @Given("^User is on NetBanking landing page$")
     public void user_is_on_netbanking_landing_page() throws Throwable {
 //        throw new PendingException();
+        System.out.println("The given section");
     }
 
     @When("^User logs into the application with username and password$")
     public void user_logs_into_the_application_with_username_and_password() throws Throwable {
 //        throw new PendingException();
+        System.out.println("The when section");
     }
 
     @Then("^Home page is populated$")
     public void home_page_is_populated() throws Throwable {
 //        throw new PendingException();
+        System.out.println("The then section");
     }
 
     @And("^Cards are displayed$")
     public void cards_are_displayed() throws Throwable {
 //        throw new PendingException();
+        System.out.println("The and section");
     }
 
 }
